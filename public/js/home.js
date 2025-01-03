f48efde9161897698298b58a9e1dc50d230869f3
Guilherme <guilhermefortunato671@gmail.com>
2025-01-03 20:01:43 -0300

    feat: create home page, css and jquery scroll redirect

---
+++ public/js/home.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    
+    /////////////////
+    // Scroll Buttons
+    const ScrollButtons = $('[scroll-target]');
+
+    ScrollButtons.each((_, button)=>{
+
+        $(button).on('click', ()=>{
+
+            scroll({'top': $(`.${$(button).attr('scroll-target')}`).position().top - 100, 'behavior': 'smooth'});
+
+        });
+
+    });
+
+});
